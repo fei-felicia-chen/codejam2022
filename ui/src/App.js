708f265a46431c4a66f12157844a298a6aa56a29
Chen Jun Chi <93779005+MosinLover@users.noreply.github.com>
2022-11-19 00:37:39 -0500

    react
    react template

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>Face recognition</h1>
+      <Todo text="Check In" />
+      <Todo text="Check Out" />
+    </div>
+  );
+}
+
+export default App;
