739db81b3becfaf9991c3251184bdb64f1df5aa3
luckypolo <michael.xi1029@gmail.com>
2022-11-19 14:28:47 -0500

    v1

---
+++ backend/findface/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <section>
+      <h1>Face Authentication by FaceIO</h1>
+      <button>Sign-in</button>
+      <button>Log-in</button>
+    </section>
+  );
+}
+
+export default App;
