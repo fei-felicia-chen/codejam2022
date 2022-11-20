aeb953c22728a66bc46fe23d48b8cc9879a589d7
Lobo808 <jlsun808@gmail.com>
2022-11-19 22:56:07 -0500

    Update location.js

--- backend/location.js
+++ backend/location.js
@@ -1,6 +1,27 @@
 let targetLong;
 let targetLat;
 
+function setLocation(long, lat) {
+  targetLat = lat;
+  targetLong = long;
+}
+
+function calcDist(long1, lat1, long2, lat2) {
+  long1 = (long1 * Math.PI) / 180;
+  lat1 = (lat1 * Math.PI) / 180;
+  long2 = (long2 * Math.PI) / 180;
+  lat2 = (lat2 * Math.PI) / 180;
+
+  let dlong = long2 - long1;
+  let dlat = lat2 - lat1;
+  let a =
+    Math.pow(Math.sin(dlat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlong / 2), 2);
+  let c = 2 * Math.asin(Math.sqrt(a));
+  let r = 6371;
+  return c * r;
+}
+
 const options = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -9,8 +30,8 @@ const options = {
 const sucessCallBack = (position) => {
   const long = position.coords.longitude;
   const lat = position.coords.latitude;
-  if (Math.abs(long - targetLong) < 1000 && Math.abs(lat - targetLat) < 1000)
-    return true;
+  let dist = calcDist(long, lat, targetLong, targetLat);
+  if (dist < 1) return true;
   else return false;
 };
 
@@ -18,8 +39,38 @@ const errorCallBack = (error) => {
   console.log(error);
 };
 
-navigator.geolocation.getCurrentPosition(
+const valid = navigator.geolocation.getCurrentPosition(
   sucessCallBack,
   errorCallBack,
   options
 );
+
+function isValidIn(long, lat) {
+  setLocation(long, lat);
+  const valid = navigator.geolocation.getCurrentPosition(
+    sucessCallBack,
+    errorCallBack,
+    options
+  );
+  if (valid) {
+    document.write("Check in success!");
+  } else {
+    document.write("Check in failed, please be at the assigned location.");
+  }
+  setTimeout(window.history.back(), 5000);
+}
+
+function isValidOut() {
+  setLocation(long, lat);
+  const valid = navigator.geolocation.getCurrentPosition(
+    sucessCallBack,
+    errorCallBack,
+    options
+  );
+  if (valid) {
+    document.write("Check out success!");
+  } else {
+    document.write("Check out failed, please be at the assigned location.");
+  }
+  setTimeout(window.history.back(), 5000);
+}
